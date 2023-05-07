58996b2becedfde88389766bd1ad4b726f462294
GP84DN <safak.tarazan@ing.com>
2023-05-07 12:23:32 +0200

    sample-stack

--- main.tf
+++ main.tf
@@ -2,18 +2,22 @@
 terraform {
   required_providers {
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    spacelift = {
-    source = "spacelift-io/spacelift" }
-
-  }
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = "~> 4.0"
+    # }
 }
 
-resource "spacelift_stack" "this" {
-  name           = "sample-stack"
-  repository     = "sample-stack-1"
-  branch         = "main"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "safak-1-s3-sample-stack"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
 }
