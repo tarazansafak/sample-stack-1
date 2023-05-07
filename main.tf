c3193337780a9234860a86beea1c99e7543db440
GP84DN <safak.tarazan@ing.com>
2023-05-07 12:51:08 +0200

    init

--- main.tf
+++ main.tf
@@ -1,23 +1,8 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket343"
 
-terraform {
-  required_providers {
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-}
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "safak-1-s3-sample-stack"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
