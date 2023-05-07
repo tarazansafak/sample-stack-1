6d6ae762367fe3ed1c4e228aae98573df7093f4a
GP84DN <safak.tarazan@ing.com>
2023-05-07 12:57:33 +0200

    init

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
+
+terraform {
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+}
+
+provider "aws" {
+  region     = "eu-west-1"
+}
+
+
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket343"
+  bucket = "my-tf-test-bucket23"
 
   tags = {
     Name        = "My bucket"
